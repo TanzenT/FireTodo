9a673f3f667ae8226fca99ff1832a4d7434a6d0e
TanzenT <30624945+TanzenT@users.noreply.github.com>
2020-03-30 17:09:22 +0900

    asdf

--- lib/main.dart
+++ lib/main.dart
@@ -3,18 +3,27 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of Fire Todo.
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
         // Set main color to Red for Lightmode
+        primaryColor: Colors.red[400],
         primarySwatch: Colors.red,
+        primaryTextTheme: Typography(platform: TargetPlatform.iOS).white,
+        textTheme: Typography(platform: TargetPlatform.iOS).white,
+        primaryIconTheme: IconThemeData(color: Colors.white),
       ),
+
       // set main color to Red for Darkmode
       darkTheme: ThemeData.dark().copyWith(
-        primaryColor: Colors.red,
+        primaryColor: Colors.red[400],
       ),
       home: App(),
     );
--- lib/src/App.dart
+++ lib/src/App.dart
@@ -1,19 +1,78 @@
-import 'package:firetodo/src/activity/MainActivity.dart';
+import 'package:firetodo/src/page/AddTodoPage.dart';
+import 'package:firetodo/src/page/ShowNonAlertPage.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_icons/flutter_icons.dart';
 
-class App extends StatelessWidget {
+import 'page/ShowAlertPage.dart';
+
+class App extends StatefulWidget {
+  @override
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> with SingleTickerProviderStateMixin {
+  TabController ctr;
+
+  @override
+  void initState() {
+    super.initState();
+    ctr = new TabController(vsync: this, length: 3);
+  }
+
+  @override
+  void dispose() {
+    ctr.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    // return Material thingy
     return Scaffold(
       appBar: AppBar(
+        brightness: Brightness.dark, // for white status bar text
         // this is for AppBar Title
+        elevation: 0.0,
         title: Row(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[Icon(Icons.extension), Text('Fire Todo')],
+          children: <Widget>[
+            Icon(MaterialCommunityIcons.fire),
+            Padding(
+              padding: EdgeInsets.only(right: 5),
+            ),
+            Text('Fire Todo')
+          ],
         ),
       ),
-      body: MainActivity(),
+      bottomNavigationBar: SafeArea(
+        child: TabBar(
+          indicatorColor: Colors.transparent,
+          unselectedLabelColor: Color.fromRGBO(0, 0, 0, 1),
+          controller: ctr,
+          tabs: <Tab>[
+            new Tab(
+              icon: new Icon(Icons.add),
+              text: '추가',
+            ),
+            new Tab(
+              icon: new Icon(Icons.timer),
+              text: '알림 있는 할 일',
+            ),
+            new Tab(
+              icon: new Icon(Icons.timer_off),
+              text: '알림 없는 할 일',
+            ),
+          ],
+        ),
+      ),
+      backgroundColor: Color.fromRGBO(186, 104, 200, 1),
+      body: new TabBarView(
+        controller: ctr,
+        children: <Widget>[
+          new AddTodoPage(),
+          new ShowAlertPage(),
+          new ShowNonAlertPage(),
+        ],
+      ),
     );
   }
 }
